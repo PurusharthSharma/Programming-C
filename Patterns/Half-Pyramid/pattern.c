e05e7485199f29348e8a5beb98fdb996f3972c7a
Purusharth Sharma <36877042+PurusharthSharma@users.noreply.github.com>
2019-04-14 12:54:23 +0530

    Create pattern.c

---
+++ Patterns/Half-Pyramid/pattern.c
@@ -0,0 +1,21 @@
+//        A Program to print half pyramid
+
+
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+    {
+        for(j=1; j<=i; ++j)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
