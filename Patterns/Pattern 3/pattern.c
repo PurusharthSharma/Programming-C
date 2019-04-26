022370249388e941a5f3db6d1304b08345d46392
Purusharth Sharma <36877042+PurusharthSharma@users.noreply.github.com>
2019-04-26 21:23:05 +0530

    Create pattern.c

---
+++ Patterns/Pattern 3/pattern.c
@@ -0,0 +1,52 @@
+#include<stdio.h> 
+pattern(int n) 
+{ 
+    int i,j; 
+      
+  
+    for (i=1; i<=n; i++) 
+    { 
+        for (j=1; j<=(2*n); j++) 
+        { 
+            // Left part of pattern 
+            if (i<j) 
+                printf(" "); 
+            else
+                printf("*"); 
+              
+          
+            if (i<=((2*n)-j)) 
+                printf(" "); 
+            else
+                printf("*"); 
+        } 
+        printf("\n"); 
+    } 
+      
+   
+    for (i=1; i<=n; i++) 
+    { 
+        for (j=1;j<=(2*n);j++) 
+        { 
+            
+            if (i>(n-j+1)) 
+                printf(" "); 
+            else
+                printf("*"); 
+                  
+         
+            if ((i+n)>j) 
+                printf(" "); 
+            else
+                printf("*"); 
+        } 
+        printf("\n"); 
+    } 
+} 
+  
+
+int main() 
+{ 
+    pattern(7); 
+    return 0; 
+} 
